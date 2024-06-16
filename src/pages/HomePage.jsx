b9753f53b9ebcf1eb59c5d3181c381f32e12c746
Angela Montanez <angelamountains@gmail.com>
2024-06-16 19:52:40 +0200

    centered text and button on hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,12 +5,14 @@ function HomePage() {
   return (
     <>
       <div className="hero">
-        {/* <h1>Welcome!</h1>
-        <h3>Curate your taste with us</h3>
-        <p>
-          Tired of looking for music and not finding the right selection? We are
-          here to help you with that!
-        </p> */}
+        <div className="hero-text">
+          <h1>Welcome to Movietunes</h1>
+          <h3>Curate your taste with us</h3>
+          <p>
+            Tired of looking for music and not finding the right selection? We
+            are here to help you with that!{" "}
+          </p>
+        </div>
         <button>Try it now</button>
       </div>
       <div>
