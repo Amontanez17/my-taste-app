267c8c54bb6eabbbc6699c4e500ee6179f40cffa
Angela Montanez <angelamountains@gmail.com>
2024-06-16 19:35:10 +0200

    add hero bg image and button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
 import MovieList from "../components/MovieList";
+import "../App.css";
 
 function HomePage() {
   return (
-    <div>
-      <MovieList />
-    </div>
+    <>
+      <div className="hero">
+        {/* <h1>Welcome!</h1>
+        <h3>Curate your taste with us</h3>
+        <p>
+          Tired of looking for music and not finding the right selection? We are
+          here to help you with that!
+        </p> */}
+        <button>Try it now</button>
+      </div>
+      <div>
+        <MovieList />
+      </div>
+    </>
   );
 }
 
